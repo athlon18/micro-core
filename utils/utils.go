d36f825a9e3c872bd5d935fc755324d75fc16caf
athlon18 <44226614+athlon18@users.noreply.github.com>
2022-05-19 14:06:32 +0800

    Update utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -1,10 +1,17 @@
 package utils
 
-const (
+var (
 	ConfigEnv  = "APP_CONFIG"
 	ConfigFile = "config.yaml"
 )
 
 type Util struct {
+}
 
+func SetConfigEnv(str string) {
+	ConfigEnv = str
+}
+
+func SetConfigFile(str string) {
+	ConfigFile = str
 }
